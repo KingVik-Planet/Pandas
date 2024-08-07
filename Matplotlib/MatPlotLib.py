7bd7275d9af1f33a826b7234e5bba68944033cf8
KingVik Planet <chukwukingsley56@gmail.com>
2024-08-07 11:17:20 +0200

    Bar Color Analysis on Land Cover land Use

--- Matplotlib/MatPlotLib.py
+++ Matplotlib/MatPlotLib.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([4, 10, 15, 2])
-ypoints = np.array([10, 20, 3, 14])
+fig, ax = plt.subplots()
+Class = ["Water/Meshland", "Urban", "Shrubs", "Forest", "Bare Soil"]
+value = [3.083, 28.321, 16.077, 16.762, 35.757]
+bar_labels = ["cyan", "blue", "orange", "green", "red"]
+bar_colors = ["tab:cyan", "tab:blue","tab:orange","tab:green", "tab:red"]
+ax.bar(Class, value, label = bar_labels , color = bar_colors)
+ax.set_ylabel("Year 1993")
+ax.set_xlabel("Land Cover Land Use Class")
+ax.legend(title = "LCLU", loc = 2)
+ax.set_title("Land Cover Land Use Changes Year 1993")
 
-plt.plot(xpoints, ypoints, "o")
 plt.show()
