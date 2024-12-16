b0b74dfc548206c8b208e145b02a42ed8fb02430
KingVik Planet <chukwukingsley56@gmail.com>
2024-12-17 00:22:26 +0200

    Discrete Distribution as Horizontal Bar Chart

--- Matplotlib/MatPlotLib.py
+++ Matplotlib/MatPlotLib.py
@@ -1,29 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Variables - 5 Classes of Land use and Land Cover
-LandUse = ("Forest", "Shrubs", "Urban", "Water", "Grassland")
+category_names = ["strongly disagree", "disagree", "Neither agree nor disagree", "agree", "strongly agree"]
 
-# Class (2000, 2010, and 2020) with Count
-# Sum of counts for each year is 54321
-LandUseData = {
-    "2000": np.array([20000, 15000, 8000, 6321, 5000]),
-    "2010": np.array([18000, 16000, 10000, 6321, 4000]),
-    "2020": np.array([15000, 17000, 12000, 5321, 5000])
+results = {
+    "Question 1": [13, 10, 14, 20, 13],
+    "Question 2": [15, 12, 19, 23, 10],
+    "Question 3": [12, 11, 18, 22, 30],
+    "Question 4": [30, 14, 18, 12, 17],
+    "Question 5": [23, 11, 12, 21, 18]
 }
 
-width = 0.7
 
-fig, ax = plt.subplots()
-bottom = np.zeros(5)
+def survey(results, category_names):
+    """
+    Parameters
+    ----------
+    results : dict
+        This is a survey got from field survey.
+        Kingsley
+        it matches the length of *category_names*
+    category_names : list of str
+        The category labels.
+    """
+    labels = list(results.keys())
+    data = np.array(list(results.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = plt.colormaps["nipy_spectral_r"](np.linspace(0.15, 0.85, data.shape[1]))
 
-for year, data in LandUseData.items():
-    Plot = ax.bar(LandUse, data, width, label=year, bottom=bottom)
-    ax.bar_label(Plot, label_type="center")
-    bottom += data
+    fix, ax = plt.subplots(figsize=(9.2, 5))
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        rects = ax.barh(labels, widths, left=starts, height=0.5, label=colname, color=color)
+        r, g, b, _ = color
+        text_color = "white" if r * g * b < 0.5 else "blue"
+        ax.bar_label(rects, label_type="center", color=text_color)
+        ax.legend(ncols=len(category_names), bbox_to_anchor=(0, 1), loc="lower left", fontsize="small")
+    return fix, ax
 
-plt.legend()
-ax.set_ylabel("Area (hectares)")
-ax.set_xlabel("Land Use / Land Cover Class")
-ax.set_title("Land Use and Land Cover Change (2000, 2010 and 2020)")
+
+survey(results, category_names)
 plt.show()
